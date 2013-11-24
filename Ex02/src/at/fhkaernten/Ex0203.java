8d15d8fda7580941c5e8f3cfab56b619a58fbf8d
romanstingler <coolx67@gmx.at>
2013-11-24 23:49:53 +0100

    added exercise 3,4 template

---
+++ Ex02/src/at/fhkaernten/Ex0203.java
@@ -0,0 +1,13 @@
+package at.fhkaernten;
+
+public class Ex0203 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ex02/src/at/fhkaernten/Ex0204.java
@@ -0,0 +1,13 @@
+package at.fhkaernten;
+
+public class Ex0204 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
